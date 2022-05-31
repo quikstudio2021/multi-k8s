0eb9bfc2d2f25409b1e3405c37b9f0ad1528c254
Tony <ymtangab@gmail.com>
2022-05-31 10:12:57 +0100

    Fix Typo

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ ssh -o StrictHostkeyChecking=no travis@$DIGITAL_OCEAN_IP_ADDRESS << ENDSSH
     git fetch
     git pull
     docker-compose down
-    echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
     source .env
     docker-compose up
 ENDSSH
